dd7dd1568abcfac94980f8af0ebd47b76ed90399
Bryan Anderson <bryan.texasbobcat@gmail.com>
2018-12-18 17:00:30 -0600

    possible fix

--- knexfile.js
+++ knexfile.js
@@ -9,7 +9,7 @@ module.exports = {
 
   production: {
     client: 'pg',
-    connection: 'https://git.heroku.com/carrie-and-bryan-todo.git'
+    connection: 'postgres://ijcuhdsljamfjj:8b871a053fa3b272a736431195f47cd7924d6a2a6e3cd872b146cc6b8cde9ea5@ec2-23-23-235-86.compute-1.amazonaws.com:5432/ddt9memp2phe6s'
   }
 
 };
